bbd39e93462cb12950a7690f97684a6cbcdb6ed9
nasrullaw74 <nasrullaw74@gmail.com>
2024-08-14 14:00:42 +0500

    updated files!

---
+++ sanity.config.js
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'gotmaldives',
+
+  projectId: 'ek5ehxje',
+  dataset: 'production',
+
+  basePath: '/studio',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
